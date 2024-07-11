9d24dd84e101568555f30797b95483a77c624315
Hudson S. Borges <hudsonsilbor@gmail.com>
2024-07-11 10:05:37 -0300

    refactor: renomeia parametro de obtencao de repositorio

--- src/github/repository.ts
+++ src/github/repository.ts
@@ -5,15 +5,15 @@ import { Repository, repositorySchema } from '../entities/repository.js';
  * Get a repository by owner and name.
  *
  * @param owner - The owner of the repository.
- * @param repo - The name of the repository.
+ * @param name - The name of the repository.
  * @param props - The properties to pass to the function.
  */
 export async function get(
   owner: string,
-  repo: string,
+  name: string,
   props: { client: Octokit }
 ): Promise<Repository | undefined> {
-  return props.client.repos.get({ owner, repo }).then((response) => {
+  return props.client.repos.get({ owner, repo: name }).then((response) => {
     if (response.status !== 200) return undefined;
     return repositorySchema.parse(response.data);
   });
