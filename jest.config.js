ce9f4f05bdbecc120dde58f53e1a5977d383ebaf
Hudson S. Borges <hudsonsilbor@gmail.com>
2024-07-16 18:11:49 -0300

    chore: adiciona limite ao jest

--- jest.config.js
+++ jest.config.js
@@ -10,5 +10,6 @@ export default {
     // '^.+\\.[tj]sx?$' to process ts,js,tsx,jsx with `ts-jest`
     // '^.+\\.m?[tj]sx?$' to process ts,js,tsx,jsx,mts,mjs,mtsx,mjsx with `ts-jest`
     '^.+\\.tsx?$': ['ts-jest', { useESM: true }]
-  }
+  },
+  workerIdleMemoryLimit: '512MB'
 };
